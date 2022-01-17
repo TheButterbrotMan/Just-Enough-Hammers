a13aa2f88ca9bab27d4522f7a983cacac7b10b29
TheButterbrotMan <64548098+TheButterbrotMan@users.noreply.github.com>
2022-01-17 22:22:52 +0100

    Added Vanillaoreplates
    Added Gold-,Netherite-,Redstone-,Lapis Lazuli-, Coal-, Emerald-, Iron-, Copper- and Diamondplate.

--- src/main/java/io/github/thebutterbrotman/awesomeplates/Mod/Items.java
+++ src/main/java/io/github/thebutterbrotman/awesomeplates/Mod/Items.java
@@ -9,15 +9,24 @@ import net.minecraft.util.registry.Registry;
 
 public class Items {
 
+ public static final Item EMERALD_PLATE = registerItem("emerald_plate"
+            , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
     public static final Item IRON_PLATE = registerItem("iron_plate"
+            , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
+    public static final Item COAL_PLATE = registerItem("coal_plate"
     , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
+    public static final Item COPPER_PLATE = registerItem("copper_plate"
+            , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
+    public static final Item LAPIS_LAZULI_PLATE = registerItem("lapis_lazuli_plate"
+            , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
     public static final Item GOLD_PLATE = registerItem("gold_plate"
             , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
     public static final Item DIAMOND_PLATE = registerItem("diamond_plate"
             , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
+    public static final Item REDSTONE_PLATE = registerItem("redstone_plate"
+            , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
     public static final Item NETHERITE_PLATE = registerItem("netherite_plate"
             , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
-
     private static Item registerItem(String name, Item item){
         return Registry.register(Registry.ITEM, new Identifier(Awesomeplates.MOD_ID, name), item);
     }
