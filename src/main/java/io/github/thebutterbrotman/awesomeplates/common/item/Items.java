d86348e195fa3949ce163e89b0bc8c910b598cc0
TheButterbrotMan <butterbrotman5.12@gmail.com>
2022-01-21 17:36:12 +0100

    Fixes

--- src/main/java/io/github/thebutterbrotman/awesomeplates/common/item/Items.java
+++ src/main/java/io/github/thebutterbrotman/awesomeplates/common/item/Items.java
@@ -10,61 +10,41 @@ import net.minecraft.util.registry.Registry;
 
 public class Items {
 
-    //Tier 1
+    //Wooden
     public static final Item WOODEN_PLATE = registerItem("wooden_plate"
             , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
     public static final Item WOODEN_HAMMER = registerItem("wooden_hammer"
-            , new HammerItem(50));
+            , new HammerItem(59));
 
-    //Tier 2
+    //Stone
     public static final Item STONE_PLATE = registerItem("stone_plate"
             , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
     public static final Item STONE_HAMMER = registerItem("stone_hammer"
-            , new HammerItem(100));
-    public static final Item COAL_PLATE = registerItem("coal_plate"
-            , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
-    public static final Item COAL_HAMMER = registerItem("coal_hammer"
-            , new HammerItem(100));
+            , new HammerItem(131));
 
-    //Tier 3
+    //Iron
     public static final Item IRON_PLATE = registerItem("iron_plate"
             , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
     public static final Item IRON_HAMMER = registerItem("iron_hammer"
-            , new HammerItem(200));
-    public static final Item LAPIS_LAZULI_PLATE = registerItem("lapis_lazuli_plate"
-            , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
-    public static final Item LAPIS_LAZULI_HAMMER = registerItem("lapis_lazuli_hammer"
-            , new HammerItem(100));
-    public static final Item COPPER_PLATE = registerItem("copper_plate"
-            , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
-    public static final Item COPPER_HAMMER = registerItem("copper_hammer"
-            , new HammerItem(200));
+            , new HammerItem(250));
 
-    //Tier 4
+    //Golden
     public static final Item GOLDEN_PLATE = registerItem("golden_plate"
             , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
     public static final Item GOLDEN_HAMMER = registerItem("golden_hammer"
-            , new HammerItem(300));
-    public static final Item EMERALD_PLATE = registerItem("emerald_plate"
-            , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
-    public static final Item EMERALD_HAMMER = registerItem("emerald_hammer"
-            , new HammerItem(300));
-    public static final Item REDSTONE_PLATE = registerItem("redstone_plate"
-            , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
-    public static final Item REDSTONE_HAMMER = registerItem("redstone_hammer"
-            , new HammerItem(300));
+            , new HammerItem(59));
 
-    //Tier 5
+    //Diamond
     public static final Item DIAMOND_PLATE = registerItem("diamond_plate"
             , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
     public static final Item DIAMOND_HAMMER = registerItem("diamond_hammer"
-            , new HammerItem(600));
+            , new HammerItem(1561));
 
-    //Tier 6
+    //Netherite
     public static final Item NETHERITE_PLATE = registerItem("netherite_plate"
             , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
     public static final Item NETHERITE_HAMMER = registerItem("netherite_hammer"
-            , new HammerItem(1000));
+            , new HammerItem(2031));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(Awesomeplates.MOD_ID, name), item);
