5fe3e06b5c2d063fa6925fd8ffaf9853dc27e531
TheButterbrotMan <64548098+TheButterbrotMan@users.noreply.github.com>
2022-01-22 19:45:23 +0100

    Plate recipes

--- src/main/java/io/github/thebutterbrotman/awesomeplates/common/item/Items.java
+++ src/main/java/io/github/thebutterbrotman/awesomeplates/common/item/Items.java
@@ -12,35 +12,35 @@ public class Items {
 
     //Wooden
     public static final Item WOODEN_HAMMER = registerItem("wooden_hammer"
-            , new HammerItem(59);
+            , new HammerItem(59));
 
     //Stone
     public static final Item STONE_HAMMER = registerItem("stone_hammer"
-            , new HammerItem(131);
+            , new HammerItem(131));
 
     //Iron
     public static final Item IRON_PLATE = registerItem("iron_plate"
             , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
     public static final Item IRON_HAMMER = registerItem("iron_hammer"
-            , new HammerItem(250);
+            , new HammerItem(250));
 
     //Golden
     public static final Item GOLDEN_PLATE = registerItem("golden_plate"
             , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
     public static final Item GOLDEN_HAMMER = registerItem("golden_hammer"
-            , new HammerItem(59);
+            , new HammerItem(59));
 
     //Diamond
     public static final Item DIAMOND_PLATE = registerItem("diamond_plate"
             , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
     public static final Item DIAMOND_HAMMER = registerItem("diamond_hammer"
-            , new HammerItem("1561");
+            , new HammerItem(1561));
 
     //Netherite
     public static final Item NETHERITE_PLATE = registerItem("netherite_plate"
             , new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
     public static final Item NETHERITE_HAMMER = registerItem("netherite_hammer"
-            , new HammerItem(2031);
+            , new HammerItem(2031));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(Awesomeplates.MOD_ID, name), item);
