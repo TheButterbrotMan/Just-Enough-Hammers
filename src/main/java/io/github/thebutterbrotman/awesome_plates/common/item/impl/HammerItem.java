780aa11ba99e616f97f480cfefb7ff33c9acbd52
Tapio <mikhailtapio13468@163.com>
2022-02-10 22:40:12 +0800

    Adjustment

--- src/main/java/io/github/thebutterbrotman/awesome_plates/common/item/impl/HammerItem.java
+++ src/main/java/io/github/thebutterbrotman/awesome_plates/common/item/impl/HammerItem.java
@@ -2,16 +2,23 @@ package io.github.thebutterbrotman.awesome_plates.common.item.impl;
 
 import com.kwpugh.pugh_lib.api.CustomRecipeRemainder;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Random;
 
 public class HammerItem extends SwordItem implements CustomRecipeRemainder {
     private final int miningLevel;
+
     public HammerItem(int durability, ToolMaterial material, int attackDamage, float attackSpeed) {
         super(material, attackDamage, attackSpeed, new Item.Settings().maxDamage(durability).group(ItemGroup.TOOLS));
         miningLevel = material.getMiningLevel();
@@ -22,12 +29,19 @@ public class HammerItem extends SwordItem implements CustomRecipeRemainder {
         return true;
     }
 
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(new TranslatableText("this.is.a.localization.key").formatted(
+                //the color of the tooltip
+                Formatting.AQUA));
+    }
+
     @Override
     public ItemStack getRecipeRemainder(ItemStack stackIn) {
-        final int instability = Math.max(0, 5 - miningLevel - (int) Math.log10(stackIn.getMaxDamage()));
+        final int instability = Math.max(0, 10 - miningLevel - (int) Math.log10(stackIn.getMaxDamage()));
         final ItemStack stack = stackIn.copy();
         final Random random = new Random();
-        stack.setDamage(stack.getDamage() + 1 + random.nextInt(instability));
+        final int damageToAdd = (random.nextInt(100) * instability > 400) ? random.nextInt(50, 101) : 1 + random.nextInt(instability);
+        stack.setDamage(stack.getDamage() + damageToAdd);
 
         if (stack.getDamage() >= stack.getMaxDamage()) {
             stack.decrement(1);
