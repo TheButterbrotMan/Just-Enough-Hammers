c1ef5a343f344fd1ad626c62c9c57fa1e3e545e8
Tapio <mikhailtapio13468@163.com>
2022-02-12 15:51:06 +0800

    Item damage and tooltip

--- src/main/java/io/github/thebutterbrotman/awesome_plates/common/item/impl/HammerItem.java
+++ src/main/java/io/github/thebutterbrotman/awesome_plates/common/item/impl/HammerItem.java
@@ -8,15 +8,17 @@ import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Random;
 
@@ -34,15 +36,23 @@ public class HammerItem extends SwordItem implements CustomRecipeRemainder {
     }
 
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        final String stabilityKey = switch (Math.min(10, Math.max(0, miningLevel + (int) Math.log10(stack.getMaxDamage())))) {
+            case 0, 1, 2, 3 -> "low";
+            case 4, 5, 6 -> "medium";
+            case 7, 8, 9, 10 -> "high";
+            default -> "";
+        };
         //todo
-        tooltip.add(new TranslatableText("this.is.a.localization.key").formatted(
-                //the color of the tooltip
-                Formatting.AQUA));
+        tooltip.add(new LiteralText(
+                MessageFormat.format(
+                        new TranslatableText("tooltip.awesome_plates.hammer_1").getString(),
+                        new TranslatableText("tooltip.awesome_plates.stability." + stabilityKey).getString())) {
+        });
     }
 
     @Override
     public ItemStack getRecipeRemainder(ItemStack stackIn) {
-        final int instability = Math.max(0, 10 - miningLevel - (int) Math.log10(stackIn.getMaxDamage()));
+        final int instability = Math.min(Math.max(0, 10 - miningLevel - (int) Math.log10(stackIn.getMaxDamage())), 10);
         final ItemStack stack = stackIn.copy();
         final Random random = new Random();
         final int damageToAdd = (random.nextInt(100) * instability > 400) ? random.nextInt(50, 101) : 1 + random.nextInt(instability);
@@ -64,6 +74,7 @@ public class HammerItem extends SwordItem implements CustomRecipeRemainder {
         if (player == null) {
             return ActionResult.PASS;
         }
+        final Hand hand = context.getHand();
         final World world = player.world;
         if (world == null) {
             return ActionResult.PASS;
@@ -72,13 +83,13 @@ public class HammerItem extends SwordItem implements CustomRecipeRemainder {
         final Direction direction = context.getSide();
         final BlockPos pos = context.getBlockPos();
         if (player instanceof ServerPlayerEntity serverPlayer) {
-            tryBreakNineBlocks(world, serverPlayer, directionToInt(direction), pos, hammerStack);
+            tryBreakNineBlocks(world, serverPlayer, directionToInt(direction), pos, hammerStack, hand);
         }
 
         player.getItemCooldownManager().set(hammerStack.getItem(),
                 //below is the cool-down tick
-                20);
-        return ActionResult.SUCCESS;
+                Math.max(5, 50 - 10 * miningLevel));
+        return ActionResult.success(world.isClient);
     }
 
     public int directionToInt(Direction direction) {
@@ -89,7 +100,7 @@ public class HammerItem extends SwordItem implements CustomRecipeRemainder {
         };
     }
 
-    public void tryBreakNineBlocks(World world, ServerPlayerEntity player, int direction, BlockPos center, ItemStack hammerStack) {
+    public void tryBreakNineBlocks(World world, ServerPlayerEntity player, int direction, BlockPos center, ItemStack hammerStack, Hand hand) {
         final boolean calX = !(direction == 1);
         final boolean calY = !(direction == 2);
         final boolean calZ = !(direction == 3);
@@ -100,13 +111,13 @@ public class HammerItem extends SwordItem implements CustomRecipeRemainder {
         for (int x = 0; x <= (calX ? 2 : 0) && willContinue; x++) {
             for (int y = 0; y <= (calY ? 2 : 0) && willContinue; y++) {
                 for (int z = 0; z <= (calZ ? 2 : 0) && willContinue; z++) {
-                    willContinue = checkAndTryBreakBlock(world, player, new BlockPos(xStart + x, yStart + y, zStart + z), hammerStack);
+                    willContinue = checkAndTryBreakBlock(world, player, new BlockPos(xStart + x, yStart + y, zStart + z), hammerStack, hand);
                 }
             }
         }
     }
 
-    public boolean checkAndTryBreakBlock(World world, ServerPlayerEntity player, BlockPos pos, ItemStack hammer) {
+    public boolean checkAndTryBreakBlock(World world, ServerPlayerEntity player, BlockPos pos, ItemStack hammer, Hand hand) {
         final Block blockToCheck = world.getBlockState(pos).getBlock();
         if (blockToCheck instanceof BlockWithEntity) {
             return true;
@@ -116,13 +127,9 @@ public class HammerItem extends SwordItem implements CustomRecipeRemainder {
             return true;
         }
         world.breakBlock(pos, true, player);
-        if (hammer.damage(1, world.random, player)) {
-            hammer.decrement(1);
-            hammer.setDamage(0);
-            if (hammer.isEmpty()) {
-                player.getInventory().remove(e -> e.getItem() == hammer.getItem(), 1, player.getInventory());
-                return false;
-            }
+        if (hammer.damage(1, player.world.random, player)) {
+            player.setStackInHand(hand, ItemStack.EMPTY);
+            return false;
         }
         return true;
     }
