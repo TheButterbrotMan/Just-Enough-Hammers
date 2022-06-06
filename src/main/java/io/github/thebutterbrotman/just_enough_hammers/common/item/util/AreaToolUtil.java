2c1353a07e024f7bd57ae713ec5d07c30a2a32aa
MikhailTapio <mikhailtapio13468@163.com>
2022-06-06 12:23:36 +0800

    Consider block-tool suitability

--- src/main/java/io/github/thebutterbrotman/just_enough_hammers/common/item/util/AreaToolUtil.java
+++ src/main/java/io/github/thebutterbrotman/just_enough_hammers/common/item/util/AreaToolUtil.java
@@ -9,6 +9,7 @@ import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.PickaxeItem;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
@@ -26,34 +27,32 @@ import java.util.Map;
 public class AreaToolUtil {
     public static byte attemptBreakNeighbors(World world, PlayerEntity player, int radius, boolean obsidian) {
         byte b = 0;
-        if (!world.isClient) {
-            final Hand hand = player.getActiveHand();
-            final ItemStack stack = player.getStackInHand(hand);
-            final Map<Enchantment, Integer> enchantments = EnchantmentHelper.get(player.getMainHandStack());
-            final boolean silkTouch = enchantments.containsKey(Enchantments.SILK_TOUCH);
-            final List<BlockPos> targetBlocks = calcRay(world, player, radius);
-            b = 0;
-            for (BlockPos pos : targetBlocks) {
-                final BlockState state = world.getBlockState(pos);
-                if (!state.isToolRequired()) {
-                    continue;
-                }
-                final Block block = state.getBlock();
-                final float hardness = state.getHardness(world, pos);
-                final boolean readyForBreaking = obsidian || (hardness < 50.0F && hardness >= 0F);
-
-                if (readyForBreaking) {
-                    if (!(block instanceof BlockEntityProvider)) {
-                        world.breakBlock(pos, false);
-                        if (silkTouch) {
-                            // drop blocks
-                            world.spawnEntity(new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(block.asItem(), 1)));
-                        } else {
-                            // drops stacks
-                            Block.dropStacks(state, world, pos, null, player, stack);   // USe this version to account for enchantments on stack
-                        }
-                        b++;
+        if (world.isClient) return 0;
+        final Hand hand = player.getActiveHand();
+        final ItemStack stack = player.getStackInHand(hand);
+        if (!(stack.getItem() instanceof PickaxeItem p)) return 0;
+        final Map<Enchantment, Integer> enchantments = EnchantmentHelper.get(player.getMainHandStack());
+        final boolean silkTouch = enchantments.containsKey(Enchantments.SILK_TOUCH);
+        final List<BlockPos> targetBlocks = calcRay(world, player, radius);
+        for (BlockPos pos : targetBlocks) {
+            final BlockState state = world.getBlockState(pos);
+            if (!state.isToolRequired() || !p.isSuitableFor(state)) {
+                continue;
+            }
+            final Block block = state.getBlock();
+            final float hardness = state.getHardness(world, pos);
+            final boolean readyForBreaking = obsidian || (hardness < 50.0F && hardness >= 0F);
+            if (readyForBreaking) {
+                if (!(block instanceof BlockEntityProvider)) {
+                    world.breakBlock(pos, false);
+                    if (silkTouch) {
+                        // drop blocks
+                        world.spawnEntity(new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(block.asItem(), 1)));
+                    } else {
+                        // drops stacks
+                        Block.dropStacks(state, world, pos, null, player, stack);   // USe this version to account for enchantments on stack
                     }
+                    b++;
                 }
             }
         }
@@ -69,34 +68,30 @@ public class AreaToolUtil {
 
         final BlockHitResult blockHitResult = world.raycast(new RaycastContext(cameraPos, cameraPosWithRotation, RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, playerIn));
 
-        if (blockHitResult.getType() == HitResult.Type.BLOCK) {
-            final Direction.Axis axis = blockHitResult.getSide().getAxis();
-
-            final ArrayList<BlockPos> targetPos = new ArrayList<>();
-            for (int x = -radius; x <= radius; x++) {
-                for (int y = -radius; y <= radius; y++) {
-                    for (int z = -radius; z <= radius; z++) {
-                        if (x != 0 || y != 0 || z != 0) targetPos.add(new BlockPos(x, y, z));
-                    }
+        if (blockHitResult.getType() != HitResult.Type.BLOCK) return blockResultList;
+        final Direction.Axis axis = blockHitResult.getSide().getAxis();
+        final ArrayList<BlockPos> targetPos = new ArrayList<>();
+        for (int x = -radius; x <= radius; x++) {
+            for (int y = -radius; y <= radius; y++) {
+                for (int z = -radius; z <= radius; z++) {
+                    if (x != 0 || y != 0 || z != 0) targetPos.add(new BlockPos(x, y, z));
                 }
             }
-
-            final BlockPos origin = blockHitResult.getBlockPos();
-
-            //Depending on the side and axis, add blockPos on same plane
-            for (BlockPos pos : targetPos) {
-                if (axis == Direction.Axis.Y) {
-                    if (pos.getY() == 0) {
-                        blockResultList.add(origin.add(pos));
-                    }
-                } else if (axis == Direction.Axis.X) {
-                    if (pos.getX() == 0) {
-                        blockResultList.add(origin.add(pos));
-                    }
-                } else if (axis == Direction.Axis.Z) {
-                    if (pos.getZ() == 0) {
-                        blockResultList.add(origin.add(pos));
-                    }
+        }
+        final BlockPos origin = blockHitResult.getBlockPos();
+        //Depending on the side and axis, add blockPos on same plane
+        for (BlockPos pos : targetPos) {
+            if (axis == Direction.Axis.Y) {
+                if (pos.getY() == 0) {
+                    blockResultList.add(origin.add(pos));
+                }
+            } else if (axis == Direction.Axis.X) {
+                if (pos.getX() == 0) {
+                    blockResultList.add(origin.add(pos));
+                }
+            } else if (axis == Direction.Axis.Z) {
+                if (pos.getZ() == 0) {
+                    blockResultList.add(origin.add(pos));
                 }
             }
         }
